3800718f6ed621fd59b9a6ae81b4ac634d32caa4
Céline Eap <eapceline@gmail.com>
2023-03-22 22:44:41 +0100

    :sparkles: add form review + review in homepage

---
+++ src/Form/ReviewFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReviewFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastName', options:[
+                'label' => 'Nom'
+            ])
+            ->add('firstName', options:[
+                'label' => 'Prénom'
+            ])
+            ->add('review', options:[
+                'label' => 'Avis'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
